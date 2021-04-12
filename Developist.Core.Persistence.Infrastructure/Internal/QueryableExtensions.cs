f762f75199b4ff82e27c5665bafec026a17ce04c
jimatas <atask@hr.nl>
2021-04-12 15:14:30 +0200

    Argument checks.

--- Developist.Core.Persistence.Infrastructure/Internal/QueryableExtensions.cs
+++ Developist.Core.Persistence.Infrastructure/Internal/QueryableExtensions.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) 2021 Jim Atas. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for details.
 
+using System;
 using System.Linq;
 
 namespace Developist.Core.Persistence
@@ -9,11 +10,21 @@ namespace Developist.Core.Persistence
     {
         public static IQueryable<T> Filter<T>(this IQueryable<T> sequence, IQueryableFilter<T> filter)
         {
+            if (filter is null)
+            {
+                throw new ArgumentNullException(nameof(filter));
+            }
+
             return filter.Filter(sequence);
         }
 
         public static IQueryable<T> Paginate<T>(this IQueryable<T> sequence, IQueryablePaginator<T> paginator)
         {
+            if (paginator is null)
+            {
+                throw new ArgumentNullException(nameof(paginator));
+            }
+
             return paginator.Paginate(sequence);
         }
     }
