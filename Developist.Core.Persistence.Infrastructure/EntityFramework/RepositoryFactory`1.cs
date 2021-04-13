9fadbb21da9f5a9aae4ce5b72bded8d7229e0647
jimatas <atask@hr.nl>
2021-04-13 09:23:54 +0200

    Use ActivatorUtilities to instantiate the repository implementation.

--- Developist.Core.Persistence.Infrastructure/EntityFramework/RepositoryFactory`1.cs
+++ Developist.Core.Persistence.Infrastructure/EntityFramework/RepositoryFactory`1.cs
@@ -2,16 +2,27 @@
 // Licensed under the MIT License. See License.txt in the project root for details.
 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+using System;
 
 namespace Developist.Core.Persistence.EntityFramework
 {
     public class RepositoryFactory<TDbContext> : IRepositoryFactory<TDbContext> where TDbContext : DbContext
     {
+        private readonly IServiceProvider serviceProvider;
+
+        public RepositoryFactory(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+        }
+
         IRepository<TEntity> IRepositoryFactory.Create<TEntity>(IUnitOfWork uow) => Create<TEntity>((IUnitOfWork<TDbContext>)uow);
 
         public virtual IRepository<TEntity> Create<TEntity>(IUnitOfWork<TDbContext> uow) where TEntity : class, IEntity
         {
-            return new Repository<TEntity, TDbContext>(uow);
+            var factory = ActivatorUtilities.CreateFactory(typeof(Repository<TEntity, TDbContext>), new[] { uow.GetType() });
+            return (IRepository<TEntity>)factory(serviceProvider, new[] { uow });
         }
     }
 }
--- Developist.Core.Persistence.Infrastructure/InMemory/RepositoryFactory.cs
+++ Developist.Core.Persistence.Infrastructure/InMemory/RepositoryFactory.cs
@@ -1,13 +1,25 @@
 ﻿// Copyright (c) 2021 Jim Atas. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for details.
 
+using Microsoft.Extensions.DependencyInjection;
+
+using System;
+
 namespace Developist.Core.Persistence.InMemory
 {
     public class RepositoryFactory : IRepositoryFactory
     {
+        private readonly IServiceProvider serviceProvider;
+
+        public RepositoryFactory(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+        }
+
         public virtual IRepository<TEntity> Create<TEntity>(IUnitOfWork uow) where TEntity : class, IEntity
         {
-            return new Repository<TEntity>(uow);
+            var factory = ActivatorUtilities.CreateFactory(typeof(Repository<TEntity>), new[] { uow.GetType() });
+            return (IRepository<TEntity>)factory(serviceProvider, new[] { uow });
         }
     }
 }
