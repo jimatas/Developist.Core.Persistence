b0193754ca636643be625ff99f25c9ed9e957ece
jimatas <atask@hr.nl>
2021-04-14 11:34:51 +0200

    Unit tests for DisposableBase and rearranged fixtures folder structure.

---
+++ Developist.Core.Persistence.Tests/Fixture/DisposableResourceSpy.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) 2021 Jim Atas. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for details.
+
+using System.Threading.Tasks;
+
+namespace Developist.Core.Persistence.Tests
+{
+    public class DisposableResourceSpy : DisposableBase
+    {
+        public int ReleaseManagedResourcesCallCount { get; private set; }
+        public int ReleaseNativeResourcesCallCount { get; private set; }
+        public int ReleaseManagedResourcesAsyncCallCount { get; private set; }
+
+        protected override void ReleaseManagedResources()
+        {
+            ReleaseManagedResourcesCallCount++;
+            base.ReleaseManagedResources();
+        }
+
+        protected override void ReleaseNativeResources()
+        {
+            ReleaseNativeResourcesCallCount++;
+            base.ReleaseNativeResources();
+        }
+
+        protected override ValueTask ReleaseManagedResourcesAsync()
+        {
+            ReleaseManagedResourcesAsyncCallCount++;
+            return base.ReleaseManagedResourcesAsync();
+        }
+    }
+}
