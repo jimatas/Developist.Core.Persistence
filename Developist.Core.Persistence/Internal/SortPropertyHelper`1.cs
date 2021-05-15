6a28c135a4b3d0a024b54c7bf53900c9aa3b4de2
jimatas <atask@hr.nl>
2021-05-15 13:29:33 +0200

    Argument check and version bump to rc-2.

--- Developist.Core.Persistence/Internal/SortPropertyHelper`1.cs
+++ Developist.Core.Persistence/Internal/SortPropertyHelper`1.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) 2021 Jim Atas. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for details.
 
+using Developist.Core.Utilities;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,12 +12,14 @@ namespace Developist.Core.Persistence
     internal static class SortPropertyHelper<T>
     {
         /// <summary>
-        /// Parses a string such as <c>"-DateOfBirth,FamilyName"</c> to an enumerable collection of <see cref="SortProperty{T}"/> objects.
+        /// Parses a string such as <c>"-DateOfBirth,FamilyName"</c> to the corresponding collection of <see cref="SortProperty{T}"/> objects.
         /// </summary>
-        /// <param name="value">A string containing potentially multiple sorting directives.</param>
+        /// <param name="value">A string containing one or more sorting directives.</param>
         /// <returns>An enumerable collection of <see cref="SortProperty{T}"/> objects parsed from the input string.</returns>
         public static IEnumerable<SortProperty<T>> ParseFromString(string value)
         {
+            Ensure.Argument.NotNullOrWhiteSpace(value, nameof(value));
+
             foreach (var directive in value.Split(',').Select(s => s.Trim()).Where(s => s != string.Empty))
             {
                 var property = directive;
@@ -26,9 +30,9 @@ namespace Developist.Core.Persistence
                     property = property.Substring(1).TrimStart('(').TrimEnd(')');
                 }
 
-                if (property == string.Empty)
+                if (property.Trim() == string.Empty)
                 {
-                    throw new FormatException("At least one of the sorting directives contained in the supplied string could not be parsed successfully.");
+                    throw new FormatException("At least one of the sorting directives in the input string could not be parsed successfully.");
                 }
 
                 var direction = descending ? SortDirection.Descending : SortDirection.Ascending;
