c696a89ee5a4dddef0a4cafc346dfd5510c97fcb
jimatas <atask@hr.nl>
2021-05-17 12:03:47 +0200

    XMLdoc comments.

--- Developist.Core.Persistence/SortingAndPagination/PaginatedList`1.cs
+++ Developist.Core.Persistence/SortingAndPagination/PaginatedList`1.cs
@@ -15,6 +15,11 @@ namespace Developist.Core.Persistence
     /// <typeparam name="T">The type of the list items.</typeparam>
     public class PaginatedList<T> : IPaginatedList<T>
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PaginatedList{T}"/> class given the current page of results and the paginator by which it was created.
+        /// </summary>
+        /// <param name="currentPage">An enumerable collection with the current page of results.</param>
+        /// <param name="paginator">The instructions by which the result set is being paginated over.</param>
         public PaginatedList(IEnumerable<T> currentPage, SortingPaginator<T> paginator)
         {
             if (currentPage is not IList<T> list)
@@ -25,18 +30,33 @@ namespace Developist.Core.Persistence
             Paginator = Ensure.Argument.NotNull(paginator, nameof(paginator));
         }
 
+        /// <summary>
+        /// The current page of items in the result set.
+        /// </summary>
         protected IList<T> InnerList { get; }
+
+        /// <summary>
+        /// The sorting and pagination instructions whereby the result set is being paginated over.
+        /// </summary>
         protected SortingPaginator<T> Paginator { get; }
 
+        /// <inheritdoc/>
         public int PageNumber => Paginator.PageNumber;
+        /// <inheritdoc/>
         public int PageSize => Paginator.PageSize;
+        /// <inheritdoc/>
         public int PageCount => Paginator.PageCount;
+        /// <inheritdoc/>
         public int ItemCount => Paginator.ItemCount;
 
         #region IReadOnlyList<T> members
+        /// <inheritdoc/>
         public T this[int index] => InnerList[index];
+        /// <inheritdoc/>
         public int Count => InnerList.Count;
+        /// <inheritdoc/>
         public IEnumerator<T> GetEnumerator() => InnerList.GetEnumerator();
+        /// <inheritdoc/>
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
         #endregion
     }
